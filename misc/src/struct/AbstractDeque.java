d1f5647329254e4f73268ced1067b4c3ab526b24
oreissig <oreissig+github@gmail.com>
2014-04-06 21:17:10 +0200

    add some more methods

--- misc/src/struct/AbstractDeque.java
+++ misc/src/struct/AbstractDeque.java
@@ -2,10 +2,70 @@ package struct;
 
 import java.util.AbstractQueue;
 import java.util.Deque;
+import java.util.Iterator;
 import java.util.NoSuchElementException;
 
 public abstract class AbstractDeque<E> extends AbstractQueue<E>
 		implements Deque<E> {
+
+	@Override
+	public E pollFirst() {
+		return poll(iterator());
+	}
+
+	@Override
+	public E pollLast() {
+		return poll(descendingIterator());
+	}
+
+	private E poll(Iterator<E> i) {
+		if (i.hasNext()) {
+			E e = i.next();
+			i.remove();
+			return e;
+		} else {
+			return null;
+		}
+	}
+
+	@Override
+	public E peekFirst() {
+		return peek(iterator());
+	}
+
+	@Override
+	public E peekLast() {
+		return peek(descendingIterator());
+	}
+
+	private E peek(Iterator<E> i) {
+		if (i.hasNext()) {
+			return i.next();
+		} else {
+			return null;
+		}
+	}
+
+	@Override
+	public boolean removeFirstOccurrence(Object o) {
+		return remove(o, iterator());
+	}
+
+	@Override
+	public boolean removeLastOccurrence(Object o) {
+		return remove(o, descendingIterator());
+	}
+
+	private boolean remove(Object o, Iterator<E> i) {
+		while (i.hasNext()) {
+			E e = i.next();
+			if (o == null ? e == null : o.equals(e)) {
+				i.remove();
+				return true;
+			}
+		}
+		return false;
+	}
 
 	@Override
 	public void addFirst(E e) {
